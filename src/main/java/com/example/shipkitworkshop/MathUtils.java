4c6ae532b8e46030e6e0ad2d42b4babbdd63b24e
Karol Wrótniak <karol.wrotniak@droidsonroids.pl>
2019-04-02 19:02:56 +0200

    Update MathUtils.java

--- src/main/java/com/example/shipkitworkshop/MathUtils.java
+++ src/main/java/com/example/shipkitworkshop/MathUtils.java
@@ -3,7 +3,8 @@ package com.example.shipkitworkshop;
 public class MathUtils {
 
     public static boolean isTriangle(int a, int b, int c) {
-        return a + b > c
+        //test
+        return a + b + 0 > c
                 && a + c > b
                 && b + c > a;
     }
